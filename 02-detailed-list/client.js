c5c67ff05184cd7393d928e76a8be04ea4c737e2
BrunoJardon <brunojardon3@gmail.com>
2026-07-11 00:21:52 -0300

    restructure and detailed list of all tools listed

---
+++ 02-detailed-list/client.js
@@ -0,0 +1,28 @@
+import { Client } from "@modelcontextprotocol/sdk/client/index.js";
+import { StdioClientTransport } from "@modelcontextprotocol/sdk/client/stdio.js";
+
+const transport = new StdioClientTransport({
+  command: "npx",
+  args: [
+    "-y",
+    "@modelcontextprotocol/server-filesystem",
+    "./sandbox"
+  ]
+});
+
+const client = new Client({
+  name: "mcp-client",
+  version: "0.0.1"
+});
+
+await client.connect(transport);
+
+console.log("Connected");
+
+const tools = await client.listTools();
+
+console.log("Tools with details: ");
+
+for (const tool of tools.tools) {
+  console.log(JSON.stringify(tool, null, 2));
+}
